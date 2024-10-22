c2a89ae15e8132dff1136fd00806bd12355daf5a
shakiqurrahman <rahmanshakiqur@gmail.com>
2024-10-22 21:31:31 +0600

    bug fixed

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,7 +2,7 @@ import localFont from "next/font/local";
 import "./globals.css";
 
 const adorNoirrit = localFont({
-    src: "./fonts/LiAdorNoirritAV1VR-VF.ttf",
+    src: "../../public/fonts/LiAdorNoirritAV1VR-VF.ttf",
     variable: "--font-ador-noirrit",
     weight: "100 900",
 });
@@ -15,7 +15,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
     return (
         <html lang="en" className={`${adorNoirrit.variable}`}>
-            <body></body>
+            <body>
+                <h1>Hello world</h1>
+            </body>
         </html>
     );
 }
