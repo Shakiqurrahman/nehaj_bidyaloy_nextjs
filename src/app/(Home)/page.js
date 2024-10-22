8d6d0b274d212fabd12dad593ddc5c8750a05ac0
shakiqurrahman <rahmanshakiqur@gmail.com>
2024-10-22 18:43:33 +0600

    fonts added

---
+++ src/app/(Home)/page.js
@@ -0,0 +1,10 @@
+export default function Home() {
+    return (
+        <div className="container mx-auto px-4 mt-10">
+            <h1>
+                জন্মান্ধ এক অভিশপ্ত সিবিলের নিঃশ্বাসে ছাওয়া বন। দুঃস্বপ্ন দিয়ে
+                ঘুম আর জাগরণের সেতুবন্ধ রচনা করা হয়েছে এখানে।{" "}
+            </h1>
+        </div>
+    );
+}
