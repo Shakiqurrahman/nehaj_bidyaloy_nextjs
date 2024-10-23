2593e067fa07a26c02c8f862f541775d7074f7fa
shakiqurrahman <rahmanshakiqur@gmail.com>
2024-10-23 19:32:01 +0600

    footer done

--- src/components/shared/Footer.jsx
+++ src/components/shared/Footer.jsx
@@ -1,9 +1,11 @@
 import Link from "next/link";
+import { FaFacebookF, FaInstagram, FaYoutube } from "react-icons/fa";
+import { FaXTwitter } from "react-icons/fa6";
 
 const Footer = () => {
     return (
         <footer className="bg-black text-white pt-10">
-            <div className="max-width text-center text-base mb-10">
+            <div className="max-width text-center text-base mb-3">
                 <p className="mb-2">
                     স্বত্ব © ২০২৪, যোগাযোগ | ই মেইল :{" "}
                     <span className="text-[#e27313]">
@@ -14,6 +16,20 @@ const Footer = () => {
                     যোগাযোগ-এ প্রকাশিত কোনো লেখা, ছবি কিংবা শিল্পকর্ম লেখক অথবা
                     কর্তৃপক্ষের অনুমতি ছাড়া কোথাও প্রকাশ করা অবৈধ ৷
                 </p>
+            </div>
+            <div className="mb-10 flex justify-center gap-2">
+                <Link href="#" target="_blank">
+                    <FaFacebookF className="bg-[#1877F2] p-2 size-8 rounded-full text-base hover:-translate-y-2 hover:opacity-85 duration-300" />
+                </Link>
+                <Link href="#" target="_blank">
+                    <FaXTwitter className="bg-[#1DA1F2] p-2 size-8 rounded-full text-base hover:-translate-y-2 hover:opacity-85 duration-300" />
+                </Link>
+                <Link href="#" target="_blank">
+                    <FaInstagram className="bg-[#262626] p-2 size-8 rounded-full text-base hover:-translate-y-2 hover:opacity-85 duration-300" />
+                </Link>
+                <Link href="#" target="_blank">
+                    <FaYoutube className="bg-[#FF0000] p-2 size-8 rounded-full text-base hover:-translate-y-2 hover:opacity-85 duration-300" />
+                </Link>
             </div>
             <p className="text-center bg-primary py-3">
                 www.nehaj-bidyaloy.com Developed by{" "}
