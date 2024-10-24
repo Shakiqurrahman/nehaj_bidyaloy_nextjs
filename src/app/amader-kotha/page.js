11c2a6530c8a2c6b718d198718372a39b053f1c6
Developer Shakil <itsdevelopershakil@gmail.com>
2024-10-24 21:07:02 +0600

    about page dummy content added

--- src/app/amader-kotha/page.js
+++ src/app/amader-kotha/page.js
@@ -1,9 +1,33 @@
+import Image from "next/image";
+import CoverPhoto from "/public/nehaz-bidyalay-logo.jpg";
+
 const AmaderKotha = () => {
-    return (
-        <div className="max-width mt-10">
-            <h1 className="text-xl">hhaa</h1>
+  return (
+    <div className="max-width my-10">
+      <h1 className="text-2xl font-ador text-center">আমাদের কথা</h1>
+      <div className="flex items-center gap-2">
+        <div className="w-1/2 text-xl">
+          <p className="mb-5">
+            যোগাযোগ। শিল্প, সাহিত্য, সংস্কৃতি, সমাজ, দর্শন, রাজনীতি, ইতিহাস ও
+            ভবিষ্যৎ-ভাবনা— ইত্যকার-বিষয়ক ওয়েবজিন। নির্মোহ ও নৈর্বক্তিক পন্থায়
+            আমরা সব ধরনের সৃষ্টিশীল রচনা ও শিল্পকর্ম প্রকাশ করি। এ ক্ষেত্রে লেখক
+            ও শিল্পীর বয়স কিংবা পরিচয় আমাদের কাছে মুখ্য নয়। আমরা কেবল পরিশীলিত
+            রচনা ও শৈল্পিক সৃষ্টিকর্মকেই বিবেচনা করি। রচনা ও শিল্পের শিল্পগুণই
+            আমাদের কাছে মুখ্য।
+          </p>
+          <p>
+            যোগাযোগ ঐতিহ্যের সাথে মেলবন্ধন ঘটাতে চায়; যোগাযোগ চায় আমাদের ভাষিক ও
+            বুদ্ধিবৃত্তিক পরম্পরার উত্তরাধিকার বহন করতে। যোগাযোগ পাঠ ও পাঠকের
+            সংযোগ সেতু। যোগাযোগ আমাদের পারস্পরিক যোগাযোগ-পথ। ঐতিহ্য ও পরম্পরার
+            এই নতুন সিলসিলায় আপনাদের স্বাগত।
+          </p>
         </div>
-    );
+        <div className="w-1/2">
+          <Image src={CoverPhoto} alt="Nehaj-Bidyalot cover photo" />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default AmaderKotha;
