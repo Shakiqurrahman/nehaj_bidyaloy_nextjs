719377025039d0de1a7ff371da0ca8a10474f0a0
shakiqurrahman <rahmanshakiqur@gmail.com>
2024-10-27 17:50:16 +0600

    dynamic added

--- src/app/authors/[userName]/page.js
+++ src/app/authors/[userName]/page.js
@@ -1,5 +1,12 @@
-const page = () => {
-  return <div>page</div>;
+'use client'
+import { useParams } from "next/navigation";
+
+const AuthorDeatils = ({ params }) => {
+    const paramsss = useParams();
+    console.log(paramsss);
+
+    // const { userName } = await params;
+    return <div>hello {paramsss.userName}</div>;
 };
 
-export default page;
+export default AuthorDeatils;
