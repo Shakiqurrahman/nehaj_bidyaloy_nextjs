6afda51a29e1d24f3886aab0a6f0ea5e8f37dd5a
Shakiqurrahman <rahmanshakiqur@gmail.com>
2024-11-01 19:53:28 +0600

    rtk query initialized

---
+++ src/Redux/api/userApiSlice.js
@@ -0,0 +1,22 @@
+import { api } from "@/config/config";
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+export const userApiSlice = createApi({
+    reducerPath: "user",
+    baseQuery: fetchBaseQuery({
+        baseUrl: `${api}`,
+        prepareHeaders: (headers) => {
+            const token = Cookies.get("authToken");
+            if (token) {
+                headers.set("Authorization", `Bearer ${token}`);
+            }
+            return headers;
+        },
+    }),
+    endpoints: (builder) => ({
+        fetchUserData: builder.query({
+            query: () => "/user",
+        }),
+    }),
+});
+
+export const { useFetchUserDataQuery } = userApiSlice;
--- src/Redux/store.js
+++ src/Redux/store.js
@@ -1,10 +1,17 @@
 import user from "@/Redux/features/userSlice";
 import utilsSlice from "@/Redux/features/utilsSlice";
 import { configureStore } from "@reduxjs/toolkit";
+import { userApiSlice } from "./api/userApiSlice";
 
 export const store = configureStore({
     reducer: {
         user: user,
         utils: utilsSlice,
+        [userApiSlice.reducerPath]: userApiSlice.reducer,
     },
+    middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware({
+            serializableCheck: false, // To allow non-serializable values
+            immutableCheck: false,
+        }).concat(userApiSlice.middleware),
 });
